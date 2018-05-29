dd988335012189d27a40616b9fd46cdc841547d2
Mevin Babu <mevinbabuc@gmail.com>
2018-05-29 15:29:14 +0530

    user model tweaks

--- junction/profiles/models.py
+++ junction/profiles/models.py
@@ -7,6 +7,12 @@ class Profile(AuditModel):
     '''
     It stores the City/Phone Details of the User.
     '''
+
+    User._meta.get_field("first_name").max_length = 100
+    User._meta.get_field("last_name").max_length = 100
+    User._meta.get_field("username").max_length = 100
+    User._meta.get_field("email").max_length = 100
+
     user = models.OneToOneField(User)
     city = models.CharField(max_length=100, blank=True, null=True)
     contact_no = models.CharField(max_length=15, blank=True, null=True)
